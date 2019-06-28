565277e4aa72e7062a04e4da0e01d1273e8c0ed8
Michael Badeen <Michael.Badeen@autozone.com>
2019-06-28 15:36:33 -0400

    Adding lint fix option

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,7 @@ const tasks = hooksArray => hooksArray.join(' && ')
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks(['yarn lint', 'yarn csslint']),
+    'pre-commit': tasks(['yarn lint --fix', 'yarn csslint']),
     'post-checkout': 'yarn install',
     'post-merge': 'yarn install',
     'post-rewrite': 'yarn install'
